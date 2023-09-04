23b674dcf55eabce60a465022d666db4ee85946e
wTait <>
2023-09-03 17:37:04 -0700

    register models with admin site

--- qset/quantum_set/admin.py
+++ qset/quantum_set/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import QuantumStateCard, QuantumOperationCard
 
-# Register your models here.
+admin.site.register(QuantumStateCard)
+admin.site.register(QuantumOperationCard)
