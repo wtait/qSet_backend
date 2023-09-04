84df2a2fd1df71443d45f7d7acfef347d002fb77
wTait <>
2023-09-03 17:35:57 -0700

    implement initial card models

--- qset/quantum_set/models.py
+++ qset/quantum_set/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class QuantumStateCard(models.Model):
+    card_id = models.AutoField(primary_key=True)
+    theta = models.FloatField()  # Angle from z-axis
+    phi = models.FloatField()    # Angle from x-axis
+
+    def __str__(self):
+        return f"QuantumStateCard {self.card_id}: Theta {self.theta}, Phi {self.phi}"
+
+class QuantumOperationCard(models.Model):
+    OPERATION_CHOICES = [
+        ('X', 'Pauli-X'),
+        ('Y', 'Pauli-Y'),
+        ('Z', 'Pauli-Z'),
+        ('H', 'Hadamard'),
+        # Add more as needed
+    ]
+    card_id = models.AutoField(primary_key=True)
+    operation = models.CharField(max_length=20, choices=OPERATION_CHOICES)
+
+    def __str__(self):
+        return f"QuantumOperationCard {self.card_id}: Operation {self.operation}"
